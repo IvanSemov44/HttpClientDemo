332eba94feb7528920c1c590627a5fbfbcdff3f6
Ivan Semo <Ivan.semov.44@gmail.com>
2021-03-09 20:18:02 +0200

    Add project files.

---
+++ HttpClientDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string NewLine = "\r\n";
+
+            TcpListener tcpListener = new TcpListener(
+                IPAddress.Loopback, 12345);
+            tcpListener.Start();
+
+            while (true)
+            {
+                var client = tcpListener.AcceptTcpClient();
+                using (var stream = client.GetStream())
+                {
+                    byte[] buffer = new byte[1000000];
+                    var lenght = stream.Read(buffer, 0, buffer.Length);
+
+                    string requestString = Encoding.UTF8.GetString(buffer, 0, lenght);
+
+                    Console.WriteLine(requestString);
+
+                    string html = $"<h1>Hello from IvanServer {DateTime.Now}</h1>";
+
+                    string response = "HTTP/1.1 200 OK" + NewLine +
+                        "Server: IvanServer 2021" + NewLine +
+                        "Content-Type: text/html; charset=utf-8" + NewLine +
+                        "Content-lenght: " + html.Length + NewLine +
+                        NewLine +
+                        html +
+                        NewLine;
+
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+                    stream.Write(responseBytes);
+
+                    //stream.Write();
+
+                    Console.WriteLine(new string('=', 70));
+                }
+            }
+        }
+
+        private static async Task ReadData()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string url = "https://softuni.bg/trainings/3164/csharp-web-basics-september-2020#lesson-18193";
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(string.Join(Environment.NewLine,
+                response.Headers.Select(x => x.Key + ": " + x.Value.First())));
+        }
+    }
+}
